841e53612f6d100cdb58ecb72d77d5ad26cb8e14
Martin Heidegger <martin.heidegger@gmail.com>
2021-10-31 03:30:26 +0900

    tests: Removing forever dependency as forever isn't maintained and 100% node14 compatible

--- test/etherrpc.js
+++ test/etherrpc.js
@@ -1,19 +1,48 @@
-const TestRPC = require('ganache-core')
-const users = require('./fixtures/users')
-const { blocktime } = require('./constants')
-const { network, constants, port } = require('./ethereum-helpers')
-const server = TestRPC.server({
-  networkId: constants.chainId,
-  blocktime: blocktime / 1000,
-  accounts: users.map(user => {
-    const key = user.keys.find(key => key.networkName === network.name && key.purpose === 'messaging')
-    return {
-      secretKey: Buffer.from(key.priv, 'hex'),
-      balance: '0x0000000000000056bc75e2d63100000'
-    }
-  })
-})
+#!/usr/bin/env node
+const cp = require('child_process')
+if (process.argv[2] === 'child') {
+  startServer()
+} else {
+  startBgProcess()
+}
 
-server.listen(port, function (err) {
-  if (err) throw err
-})
+function startBgProcess () {
+  const p = cp.fork(__filename, ['child'], {
+    stdio: 'pipe'
+  })
+  p.stdout.on('data', data => {
+    process.stdout.write(data)
+    p.unref()
+    process.exit()
+  })
+  p.stderr.on('data', err => {
+    process.stderr.write(err)
+    process.exit(1)
+  })
+}
+
+function startServer () {
+  const npid = require('npid')
+  const pid = npid.create(`${__dirname}/etherrpc.js.pid`)
+  pid.removeOnExit()
+  const TestRPC = require('ganache-core')
+  const users = require('./fixtures/users')
+  const { blocktime } = require('./constants')
+  const { network, constants, port } = require('./ethereum-helpers')
+  const server = TestRPC.server({
+    networkId: constants.chainId,
+    blocktime: blocktime / 1000,
+    accounts: users.map(user => {
+      const key = user.keys.find(key => key.networkName === network.name && key.purpose === 'messaging')
+      return {
+        secretKey: Buffer.from(key.priv, 'hex'),
+        balance: '0x0000000000000056bc75e2d63100000'
+      }
+    })
+  })
+
+  server.listen(port, function (err) {
+    console.log(`Listening ganache server at http://localhost:${port} in pid ${process.pid}`)
+    if (err) throw err
+  })
+}
